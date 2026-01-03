3447d9192869ec36ef8915cfdab716bb115ba1e4
Neha Binwal <nbinwal2@gmail.com>
2026-01-03 20:33:18 +0530

    Spring Initializr Basic Set-up

---
+++ src/main/java/com/spring_projects/spring_batch_payroll_processor/SpringBatchPayrollProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.spring_projects.spring_batch_payroll_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPayrollProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPayrollProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_projects/spring_batch_payroll_processor/SpringBatchPayrollProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_projects.spring_batch_payroll_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPayrollProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
